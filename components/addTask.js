c95ed2c470f9b432a3f12cd09e715af9a2de37b4
Sebastian <sebastiangomez101@gmail.com>
2022-11-28 18:50:32 -0300

    notas terminadas :v

--- components/addTask.js
+++ components/addTask.js
@@ -1,3 +1,4 @@
+import { uniqueDates } from '../services/date.js';
 import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js'; 
 import { displayTasks } from './readTasks.js';
@@ -21,38 +22,51 @@ export const addTask = (evento) => {
     input.value = '';
     calendar.value = '';
 
+    const complete = false;
+    
     const taskObj = {
         value, 
-        dateFormat
+        dateFormat,
+        complete,
+        id: uuid.v4()
       }; 
 
-      list.innerHTML = '';
+
+    list.innerHTML = '';
     const taskList = JSON.parse(localStorage.getItem("tasks")) || [] ;
-    taskList.push({value,dateFormat})
+    taskList.push(taskObj);
     localStorage.setItem("tasks", JSON.stringify(taskList))
-   displayTasks() 
+    displayTasks();
+   
 
    
   }
   
-
- export const createTask = ({value, dateFormat}) => {
+ export const createTask = ({value, dateFormat, complete, id}) => {
     const task = document.createElement('li');
           task.classList.add('card');
     
     //backticks
     const taskContent = document.createElement('div');
     
+    const check = checkComplete(id);
+
+    if(complete) {
+      console.log("completar")
+      check.classList.toggle('fas');
+      check.classList.toggle('completeIcon');
+      check.classList.toggle('far');
+    }
     const titleTask = document.createElement('span');
           titleTask.classList.add('task');
           titleTask.innerText = value;
-          taskContent.appendChild(checkComplete());
+          taskContent.appendChild(check);
           taskContent.appendChild(titleTask);
     // task.innerHTML = content;
     const dateElement = document.createElement("span"); 
           dateElement.innerHTML = dateFormat;
           task.appendChild(taskContent);
           task.appendChild(dateElement)
-          task.appendChild(deleteIcon());
+          task.appendChild(deleteIcon(id));
     return task;
   };
--- services/date.js
+++ services/date.js
@@ -11,3 +11,13 @@ export const uniqueDates = (tasks) => {
     
     return unique;
 }
+
+export const orderDates = (dates) => {
+    return dates.sort((a, b) => {
+        const firstDate = moment(a, "DD/MM/YYYY")
+        const secondDate = moment(b, "DD/MM/YYYY") 
+        console.log(dates);
+        return firstDate - secondDate
+       
+    });
+}
